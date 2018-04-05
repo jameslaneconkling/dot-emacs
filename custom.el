fe1f321bdec2a7a4dce228afd42b7d9bc94f6421
James Conkling <james.lane.conkling@gmail.com>
2018-04-05 00:23:49 -0400

    add javascript lang mode

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (neotree diff-hl magit-gh-pulls magit helm-projectile projectile markdown-mode helm-ag helm ido-completing-read+ smartparens rainbow-delimiters paredit cider clojure-mode-extra-font-locking clojure-mode aggressive-indent zenburn-theme company-tern tern-auto-complete tern rainbow-mode auto-complete use-package evil-visual-mark-mode)))
+    (evil-tutor indium neotree diff-hl magit-gh-pulls magit helm-projectile projectile markdown-mode helm-ag helm ido-completing-read+ smartparens rainbow-delimiters paredit cider clojure-mode-extra-font-locking clojure-mode aggressive-indent zenburn-theme company-tern tern-auto-complete tern rainbow-mode auto-complete use-package evil-visual-mark-mode)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
---
+++ lang/javascript-cfg.el
@@ -0,0 +1,8 @@
+
+;; ~/.emacs.d/personal/lang/javascript-cfg.el
+
+;;;;;;;;;;;;;;;;
+;; javascript ;;
+;;;;;;;;;;;;;;;;
+
+(use-package indium :ensure t)
