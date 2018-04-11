e4a61dcd40e7e98dc5eaf04c2a3e808aaa1404a3
James Conkling <james.lane.conkling@gmail.com>
2018-04-11 12:18:11 -0400

    add keybindings for magit

--- program/magit-cfg.el
+++ program/magit-cfg.el
@@ -9,7 +9,23 @@
 (use-package magit-gh-pulls :ensure t)
 (require 'magit-gh-pulls)
 (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
-
+(general-define-key
+ :states '(normal visual insert emacs)
+ :prefix "SPC"
+ :non-normal-prefix "M-SPC"
+ "g" '(:ignore t :which-key "git")
+ "g s" '(magit-status :which-key "status")
+ "g a" '(magit-stage :which-key "add")
+ "g d" '(magit-diff :which-key "diff")
+ "g c" '(magit-commit :which-key "commit")
+ "g o" '(magit-checkout :which-key "checkout")
+ "g l" '(magit-reflog-head :which-key "log")
+ "g /" '(:ignore t :which-key "stash")
+ "g / /" '(magit-stash :which-key "stash")
+ "g / p" '(magit-stash-pop :which-key "pop")
+ "g p" '(:ignore t :which-key "push/pull")
+ "g p s" '(magit-push :which-key "push")
+ "g p l" '(magit-pull :which-key "pull"))
 
 (use-package git-gutter-fringe
   :ensure t)
